0caf13b40abe1f89da3ffe5e1a73c53477492f68
Aluno <Aluno@fatecdiadema.com>
2024-10-01 11:15:11 -0300

    mudanças tela de cadDoacoes

--- src/pages/cadDoacao/components/dadosDoacao/index.jsx
+++ src/pages/cadDoacao/components/dadosDoacao/index.jsx
@@ -1,123 +1,114 @@
+import InputField from "../../../../components/inputs/InputField";
+import SelectField from "../../../../components/inputs/SelectField";
+
 export default function DadosDoacao(props) {
-    const { mensagens, setMensagens, doacao, setDoacao } = props
+  const { mensagens, setMensagens, doacao, setDoacao } = props;
 
-    // Categorias de doacao para preencher os dropdown
-    const categoriaDoacao = {
-        1:"Caseira",
-        2:"Industrializado",
-        3: "Perecível",
-        4: "Não Perecível",
-        5: "In Natura",
-    }
-    const formaConservacao = {
-        1: "Armazenamento em local seco",
-        2: "Pronto para consumo",
-        3: "Refrigeração",
-        4: "Congelamento",
-    }
+  // Categorias de doacao para preencher os dropdown
+  const categoriaDoacao = {
+    1: "Caseira",
+    2: "Industrializado",
+    3: "Perecível",
+    4: "Não Perecível",
+    5: "In Natura",
+  };
+  const formaConservacao = {
+    1: "Armazenamento em local seco",
+    2: "Pronto para consumo",
+    3: "Refrigeração",
+    4: "Congelamento",
+  };
 
-    return (
-        <>
-            <h3>Dados da doação:</h3>
-            <div className="form-group">
-                <label htmlFor="nome_doacao">Nome da doação:</label>
-                <input
-                    type="text"
-                    id="nome_doacao"
-                    className="form-control"
-                    placeholder="Nome doacao"
-                    defaultValue={doacao.nome}
-                    onChange={(e) => setDoacao({ ...doacao, nome: e.target.value })}
-                />
-                <small className="form-text text-muted">
-                    {mensagens.nome ?? ""}
-                </small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="qtd_alimentos">Quantidade de alimentos:</label>
-                <input
-                    type="text"
-                    id="qtd_alimentos"
-                    className="form-control"
-                    placeholder="Quantidade de alimentos"
-                    onChange={(e) => setDoacao({ ...doacao, qtd: e.target.value })}
-                />
-                <small className="form-text text-muted">
-                    {mensagens.nome ?? ""}
-                </small>
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="tipo_alimento"> Tipo do Alimento: </label>
-                <select name="tipo_alimento" id="tipo_alimento" className="form-control" onChange={(e) => setDoacao({ ...doacao, tipo: e.target.value })} defaultValue={doacao.tipo}>
-                    <option value={0}>Selecione um tipo</option>
-                    {Object.keys(categoriaDoacao).map(key => <option key={key} value={key}>{categoriaDoacao[key]}</option>)}
-                </select>
-                <small className="form-text text-muted">
-                    {mensagens.tipo ?? ""}
-                </small>
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="modo_conserv">Modo de conservação:</label>
-                <select name="modo_conserv" id="modo_conserv" className="form-control" onChange={(e) => setDoacao({ ...doacao, conservacao: e.target.value })} defaultValue={doacao.conservacao}>
-                    <option value={0}>Selecione um modo</option>
-                    {Object.keys(formaConservacao).map(key => <option key={key} value={key}>{formaConservacao[key]}</option>)}
-                </select>
-                <small className="form-text text-muted">
-                    {mensagens.tipo ?? ""}
-                </small>
-            </div>
-
-            <div className="form-group">
-                <label htmlFor="data_fabricacao">Data de fabricação:</label>
-                <input
-                    type="date"
-                    id="data_fabricacao"
-                    className="form-control"
-                    onChange={(e) => setDoacao({ ...doacao, data_fabricacao: e.target.value })}
-                />
-                <small className="form-text text-muted">
-                    {mensagens.nome ?? ""}
-                </small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="data_validade">Data de validade:</label>
-                <input
-                    type="date"
-                    id="data_validade"
-                    className="form-control"
-                    onChange={(e) => setDoacao({ ...doacao, data_validade: e.target.value })}
-                />
-                <small className="form-text text-muted">
-                    {mensagens.nome ?? ""}
-                </small>
-            </div>
-            <h3>Dados da retirada:</h3>
-            <div className="form-group">
-                <label htmlFor="data_retirada">Data máx de retirada:</label>
-                <input
-                    type="date"
-                    id="data_retirada"
-                    className="form-control"
-                    onChange={(e) => setDoacao({ ...doacao, data_retirada: e.target.value })}
-                />
-                <small className="form-text text-muted">
-                    {mensagens.nome ?? ""}
-                </small>
-            </div>
-            <div className="form-group">
-                <label htmlFor="hora_retirada">Horário max de retirada:</label>
-                <input
-                    type="time"
-                    id="hora_retirada"
-                    className="form-control"
-                    onChange={(e) => setDoacao({ ...doacao, hora_retirada: e.target.value })}
-                />
-                <small className="form-text text-muted">
-                    {mensagens.nome ?? ""}
-                </small>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <h3>Dados da doação:</h3>
+      <InputField
+        change={(e) => setDoacao({ ...doacao, nome: e.target.value })}
+        defaultValue={doacao.nome}
+        hint={mensagens.nome ?? ""}
+        disabled={false}
+        id={"nomeDoacao"}
+        label={"Nome da doação:"}
+        msg={mensagens.nome}
+        name={"nomeDoacao"}
+        type={"text"}
+      />
+      <InputField
+        change={(e) => setDoacao({ ...doacao, qtd: e.target.value })}
+        defaultValue={doacao.qtd}
+        hint={mensagens.qtd ?? ""}
+        disabled={false}
+        id={"qtdAlimentos"}
+        label={"Quantidade de alimentos:"}
+        msg={mensagens.qtd}
+        name={"qtdAlimentos"}
+        type={"number"}
+      />
+      <SelectField
+        change={(e) => setDoacao({ ...doacao, tipo: e.target.value })}
+        defaultValue={doacao.tipo}
+        disabled={false}
+        id={"tipoAlimento"}
+        label={"Tipo do Alimento"}
+        msg={mensagens.tipo ?? ""}
+        name={"tipoAlimento"}
+        options={categoriaDoacao}
+      />
+      <SelectField
+        change={(e) => setDoacao({ ...doacao, conservacao: e.target.value })}
+        defaultValue={doacao.conservacao}
+        disabled={false}
+        id={"modoConservacao"}
+        label={"Modo de Conservação"}
+        msg={mensagens.conservacao ?? ""}
+        name={"modoConservacao"}
+        options={formaConservacao}
+      />
+      <InputField
+        change={(e) => setDoacao({ ...doacao, dataFabricacao: e.target.value })}
+        defaultValue={doacao.dataFabricacao}
+        hint={mensagens.dataFabricacao ?? ""}
+        disabled={false}
+        id={"dataFabricacao"}
+        label={"Data de fabricação:"}
+        msg={mensagens.dataFabricacao}
+        name={"dataFabricacao"}
+        type={"date"}
+      />
+      <InputField
+        change={(e) => setDoacao({ ...doacao, dataValidade: e.target.value })}
+        defaultValue={doacao.dataValidade}
+        hint={mensagens.dataValidade ?? ""}
+        disabled={false}
+        id={"dataValidade"}
+        label={"Data de validade:"}
+        msg={mensagens.dataValidade}
+        name={"dataValidade"}
+        type={"date"}
+      />
+      <h3>Dados da retirada:</h3><br />
+      <InputField
+        change={(e) => setDoacao({ ...doacao, dataRetirada: e.target.value })}
+        defaultValue={doacao.dataRetirada}
+        hint={mensagens.dataRetirada ?? ""}
+        disabled={false}
+        id={"dataRetirada"}
+        label={"Data máx de retirada:"}
+        msg={mensagens.dataRetirada}
+        name={"dataRetirada"}
+        type={"date"}
+      />
+      <InputField
+        change={(e) => setDoacao({ ...doacao, horaRetirada: e.target.value })}
+        defaultValue={doacao.horaRetirada}
+        hint={mensagens.horaRetirada ?? ""}
+        disabled={false}
+        id={"horaRetirada"}
+        label={"Horário max de retirada:"}
+        msg={mensagens.horaRetirada}
+        name={"horaRetirada"}
+        type={"date"}
+      />
+    </>
+  );
 }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,18 +3,16 @@ import CadDoadores from "../pages/cadDoadores";
 import CadRecebedores from "../pages/cadRecebedores";
 import Home from "../pages/Home";
 import Login from "../pages/login";
-import PagInicial from "../pages/pagInicial";
 import Dashboard from "../pages/Dashboard";
 import CadDoacao from "../pages/cadDoacao";
 export default function AppRoutes() {
     return (
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<PagInicial />} />
+          <Route path="/" element={<Home />} />
           <Route path="/cadastro" element={<CadDoadores />} />
           <Route path="/CadRecebedores" element={<CadRecebedores />} />
           <Route path="/Login" element={<Login />} />
-          <Route path="/Home" element={<Home />} />
           <Route path="/Dashboard" element={<Dashboard />}/>
           <Route path="/CadDoacao" element={<CadDoacao />}/>
         </Routes>
