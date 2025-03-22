f23d073eb2337bb3b6d7700ea1de96b7181e425d
txr-nicole <nicolefagundes24@gmail.com>
2025-03-22 19:11:40 -0300

    Alterando posicionamento dos cards de doação

--- src/components/listaDoacoes/ListaDoacoes.jsx
+++ src/components/listaDoacoes/ListaDoacoes.jsx
@@ -1,35 +1,35 @@
-import { useContext, useEffect, useState } from "react"
-import { CardDoacao } from "./components/CardDoacao"
-import { DashboardContext } from "../../pages/Dashboard"
+import { useContext, useEffect, useState } from "react";
+import { DashboardContext } from "../../pages/Dashboard";
+import { CardDoacao } from "./components/CardDoacao";
 
 export function ListaDoacoes({ getDoacoes, params = [] }) {
-  const { doacoesAlteradas, setDoacoesAlteradas } = useContext(DashboardContext)
+  const { doacoesAlteradas, setDoacoesAlteradas } =
+    useContext(DashboardContext);
 
-  const [doacoes, setDoacoes] = useState(null)
+  const [doacoes, setDoacoes] = useState(null);
 
   useEffect(() => {
-    getListaDeDoacoes()
-  }, [doacoesAlteradas])
+    getListaDeDoacoes();
+  }, [doacoesAlteradas]);
 
   const getListaDeDoacoes = async () => {
-    let result = await getDoacoes(...params)
-    setDoacoes(result)
-  }
+    let result = await getDoacoes(...params);
+    setDoacoes(result);
+  };
 
-  return(
+  return (
     <>
-      {
-        doacoes != null && doacoes.length > 0
-        ? <div className="flex gap-2 overflow-x-auto">
-         { doacoes.map((doacao) => 
-            <CardDoacao key={doacao.id} doacao={ doacao } />
-          ) } 
+      {doacoes != null && doacoes.length > 0 ? (
+        <div className="flex justify-around flex-wrap overflow-y-auto max-h-[550px] max-w-[1400px] inset-6">
+          {doacoes.map((doacao) => (
+            <CardDoacao key={doacao.id} doacao={doacao} />
+          ))}
         </div>
-         
-        : <div className="flex flex-col gap-2 max-w-64 bg-white rounded-xl p-2 shadow-gray-300 shadow-md">
-            <p>Não há doações disponíveis</p>
-          </div>
-      }
+      ) : (
+        <div className="flex flex-col gap-2 max-w-64 bg-white rounded-xl p-2 shadow-gray-300 shadow-md">
+          <p>Não há doações disponíveis</p>
+        </div>
+      )}
     </>
-  )
+  );
 }
