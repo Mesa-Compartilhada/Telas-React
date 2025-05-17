d720dc78c1d4460046f9f46ead84e306fde0e71d
willotavio <89710130+willotavio@users.noreply.github.com>
2025-05-17 13:52:06 -0300

    Adicionando react-toastify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { BrowserRouter } from "react-router-dom";
 import { AuthWrapper } from "./auth/AuthWrapper";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer 
+        position="top-center"
+        autoClose={3000}
+        newestOnTop={true}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
       <AuthWrapper />
     </BrowserRouter>
   );
